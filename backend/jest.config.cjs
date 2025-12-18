83df89fa0b1290ac0ba810841e70588d602a7a32
Me1 <alsu230204@mail.ru>
2025-12-19 03:46:59 +0500

    Исправленное

--- backend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!bcryptjs|jsonwebtoken)', // Add more ESM packages here if needed
-  ],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'], // If you want to auto-run your setup.js
-};
